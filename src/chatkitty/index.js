bc0d9e2ff83d8a57bf68cb7da9f89cfd9bb0f284
samsamho <samsamho718@gmail.com>
2021-12-20 19:13:04 +0800

    new login and signup page with expo

---
+++ src/chatkitty/index.js
@@ -0,0 +1,3 @@
+import ChatKitty from 'chatkitty';
+
+export const kitty = ChatKitty.getInstance('YOUR CHATKITTY API KEY HERE');
---
+++ src/navigation/Routes.js
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+import AuthStack from './AuthStack';
+
+export default function Routes() {
+  return (
+      <NavigationContainer>
+        <AuthStack />
+      </NavigationContainer>
+  );
+}
