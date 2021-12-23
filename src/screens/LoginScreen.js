cbcfe4add020a57a41b81b695770a85132ab4cf4
PotatoHY <oscarmaths9141@gmail.com>
2021-12-24 00:47:22 +0800

    minor change

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -29,7 +29,6 @@ export default function LoginScreen({ navigation }) {
             modeValue="contained"
             labelStyle={styles.loginButtonLabel}
             onPress={() => {
-
             }}
         />
         <FormButton
