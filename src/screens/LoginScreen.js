3c6ca259a29f2ac879890ee92d04e96bc2edf515
PotatoHY <oscarmaths9141@gmail.com>
2021-12-29 15:06:15 +0800

    Refactor components

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Title } from 'react-native-paper';
-
-import FormButton from '../components/FormButton';
-import FormInput from '../components/FormInput';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Button, Title, TextInput } from 'react-native-paper';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
@@ -12,51 +9,72 @@ export default function LoginScreen({ navigation }) {
   return (
       <View style={styles.container}>
         <Title style={styles.titleText}>Welcome!</Title>
-        <FormInput
-            labelName="Email"
+        <TextInput
+            label="Email"
+            style={styles.input}
             value={email}
+            numberOfLines={1}
             autoCapitalize="none"
             onChangeText={(userEmail) => setEmail(userEmail)}
         />
-        <FormInput
-            labelName="Password"
+        <TextInput
+            label="Password"
+            style={styles.input}
             value={password}
+            numberOfLines={1}
             secureTextEntry={true}
             onChangeText={(userPassword) => setPassword(userPassword)}
         />
-        <FormButton
-            title="Login"
-            modeValue="contained"
+        <Button
+            mode="contained"
+            style={styles.input}
             labelStyle={styles.loginButtonLabel}
-            onPress={() => {
-            }}
-        />
-        <FormButton
-            title="Sign up here"
-            modeValue="text"
+            onPress={() => {}}
+            style={styles.button}
+            contentStyle={styles.buttonContainer}
+        > Login </Button>
+        <Button
+            mode="text"
             uppercase={false}
             labelStyle={styles.navButtonText}
             onPress={() => navigation.navigate('Signup')}
-        />
+            style={styles.button}
+            contentStyle={styles.buttonContainer}
+        > Sign up here </Button>
       </View>
   );
 }
 
+const { width, height } = Dimensions.get('screen');
+
 const styles = StyleSheet.create({
+  button: {
+    marginTop: 10,
+  },
+  buttonContainer: {
+    width: width / 2,
+    height: height / 15,
+  },
   container: {
     backgroundColor: '#f5f5f5',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  titleText: {
-    fontSize: 24,
+  input: {
+    marginTop: 10,
     marginBottom: 10,
+    width: width / 1.5,
+    height: height / 15,
   },
   loginButtonLabel: {
     fontSize: 22,
   },
   navButtonText: {
     fontSize: 16,
+  },
+  titleText: {
+    fontSize: 24,
+    marginBottom: 10,
   },
 });
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { IconButton, Title } from 'react-native-paper';
-
-import FormButton from '../components/FormButton';
-import FormInput from '../components/FormInput';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Button, IconButton, TextInput, Title } from 'react-native-paper';
 
 export default function SignupScreen({ navigation }) {
   const [displayName, setDisplayName] = useState('');
@@ -13,54 +10,71 @@ export default function SignupScreen({ navigation }) {
   return (
       <View style={styles.container}>
         <Title style={styles.titleText}>Let's get started!</Title>
-        <FormInput
-            labelName="Display Name"
+        <TextInput
+            label="Display Name"
+            style={styles.input}
             value={displayName}
             autoCapitalize="none"
             onChangeText={(userDisplayName) => setDisplayName(userDisplayName)}
         />
-        <FormInput
-            labelName="Email"
+        <TextInput
+            label="Email"
+            style={styles.input}
             value={email}
             autoCapitalize="none"
             onChangeText={(userEmail) => setEmail(userEmail)}
         />
-        <FormInput
-            labelName="Password"
+        <TextInput
+            label="Password"
+            style={styles.input}
             value={password}
             secureTextEntry={true}
             onChangeText={(userPassword) => setPassword(userPassword)}
         />
-        <FormButton
-            title="Signup"
-            modeValue="contained"
+        <Button
+            mode="contained"
             labelStyle={styles.loginButtonLabel}
             // onPress={() => register(displayName, email, password)}
-        />
+        >Sign Up</Button>
         <IconButton
+            color="#5b3a70"
+            onPress={() => navigation.goBack()}
             icon="keyboard-backspace"
             size={30}
             style={styles.navButton}
-            color="#5b3a70"
-            onPress={() => navigation.goBack()}
         />
       </View>
   );
 }
 
+const { width, height } = Dimensions.get('screen');
+
 const styles = StyleSheet.create({
+  button: {
+    marginTop: 10,
+  },
+  buttonContainer: {
+    width: width / 2,
+    height: height / 15,
+  },
   container: {
     backgroundColor: '#f5f5f5',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  titleText: {
-    fontSize: 24,
+  input: {
+    marginTop: 10,
     marginBottom: 10,
+    width: width / 1.5,
+    height: height / 15,
   },
   loginButtonLabel: {
     fontSize: 22,
+  },
+  titleText: {
+    fontSize: 24,
+    marginBottom: 10,
   },
   navButtonText: {
     fontSize: 18,
