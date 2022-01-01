899164f59e1e23c531a8035a133523cf063fdba7
Sola <kylesora419@gmail.com>
2022-01-01 12:28:31 +0800

    Basic chat function

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,16 +1,22 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import ChatScreen from '../screens/ChatScreen';
 
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 
 const Stack = createStackNavigator();
-
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: "purple"},
+  headerTitleStyle: {color:"white"},
+  headerTintColor:"white",
+}
 export default function AuthStack() {
   return (
-      <Stack.Navigator initialRouteName="Login" headerMode="none">
+      <Stack.Navigator initialRouteName="Login" screenOptions={globalScreenOptions}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen}/>
       </Stack.Navigator>
   );
 }
